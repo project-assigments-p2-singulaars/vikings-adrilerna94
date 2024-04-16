c4c73ce4f55e2d61d048bdd55eadc37d69987169
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2024-04-16 11:28:04 +0200

    saxon.ts and saxon.test.ts done

--- src/Saxon.ts
+++ src/Saxon.ts
@@ -1,1 +1,30 @@
-export class Saxon{}
+import {Soldier} from "./Soldier.ts";
+
+export class Saxon extends Soldier{
+
+    constructor(health:number, strength: number){
+        super(health,strength);
+    }
+    attack(){
+        return this.strength;
+    }
+    receiveDamage(damage: number){
+        this.health = this.health - damage;
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`;
+        }
+        else{
+            return 'A Saxon has died in combat';
+        }
+
+        // Otra forma con operador ternario
+
+        /*let msnDamage : string = this.health > 0 ? 
+        `A Saxon has received ${damage} points of damage`: 'A Saxon has died in combat';
+
+        return msnDamage;*/
+    }
+
+    
+
+}
