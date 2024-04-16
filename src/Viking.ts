ba110016e06c6a5a6884f4a8e8ff3780d9732452
adrilerna94 <you@example.comgit config --global user.email you@example.com>
2024-04-16 11:03:07 +0200

    soldier.ts and viking.ts done

--- src/Viking.ts
+++ src/Viking.ts
@@ -1,1 +1,26 @@
-export class Viking{}
+import { Soldier } from "./Soldier";
+
+export class Viking extends Soldier{
+    name : string;
+    constructor(name: string, health: number, strength:number){
+        super(health, strength);
+        this.name = name;
+    }
+
+    attack(){
+        return this.strength;
+    }
+
+    receiveDamage(damage: number) {
+        this.health -= damage; 
+
+        let healthMessage : string = this.health > 0 ? 
+        this.name + ` has received ${damage} points of damage`: this.name + ` has died in act of combat`;
+
+        return healthMessage;
+    }
+    battleCry(){
+        let odinMessage : string = "Odin Owns You All!";
+        return odinMessage;
+    }
+}
